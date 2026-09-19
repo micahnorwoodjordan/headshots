f07afda30102a6b26753c3fade267bfc18d3ee5a
Micah Norwood <micahnorwoodjordan@gmail.com>
2026-09-19 15:49:13 -0700

    slightly more polished command

--- backend/diffuse.py
+++ backend/diffuse.py
@@ -14,9 +14,12 @@ print("GPU:", torch.cuda.get_device_name(0))
 
 
 PROMPT = '''
-turn this image of me into a professional headshot.
-i should be outside in front of a cactus with a slight smile.
-preserve all of my facial and hair features
+turn this image of me into a professional headshot. preserve all of my facial and hair features. i should be:
+
+- outside
+- in front of a cactus
+- slightly smiling
+- facing 20 degrees to the left of the camera
 '''
 
 pipeline = AutoPipelineForImage2Image.from_pretrained("Qwen/Qwen-Image-Edit-2509", dtype=torch.bfloat16, device="cpu")
@@ -26,6 +29,4 @@ pipeline.to("cuda")
 print(pipeline.device)
 
 image = pipeline(image=Image.open("artifacts/input/headshot.jpg"), prompt=PROMPT).images[0]
-print(image)
-print(dir(image))
 image.save("artifacts/output/result.jpg")
